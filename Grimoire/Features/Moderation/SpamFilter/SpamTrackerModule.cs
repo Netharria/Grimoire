e0d153451bcb62276623352ecc2cf511d88c7e7f
Netharia <netharia.87@gmail.com>
2025-01-22 14:43:26 -0500

    locking fluentassertions to last free version

--- Grimoire/Features/Moderation/SpamFilter/SpamTrackerModule.cs
+++ Grimoire/Features/Moderation/SpamFilter/SpamTrackerModule.cs
@@ -11,7 +11,7 @@ namespace Grimoire.Features.Moderation.SpamFilter;
 
 public class SpamTrackerModule
 {
-    public ConcurrentDictionary<DiscordMember, SpamTracker> SpamUsers { get; set; } = new();
+    private readonly ConcurrentDictionary<DiscordMember, SpamTracker> _spamUsers = new();
 
     public CheckSpamResult CheckSpam(DiscordMessage message)
     {
@@ -20,66 +20,62 @@ public class SpamTrackerModule
 
         if (member.Permissions.HasPermission(DiscordPermissions.ManageChannels) || member.IsOwner)
             return new CheckSpamResult { IsSpam = false };
-        if (!this.SpamUsers.TryGetValue(member, out var spamTracker))
-        {
-            spamTracker = new SpamTracker();
-            if (!this.SpamUsers.TryAdd(member, spamTracker))
-                throw new ArgumentException("Tried to add spam tracker but it already existed");
-        }
 
-        if (message.Id == spamTracker.LastMesssageId)
+        var spamTracker = this._spamUsers.GetOrAdd(member, new SpamTracker());
+
+        if (message.Id == spamTracker.LastMessageId)
             return new CheckSpamResult { IsSpam = false };
 
-        spamTracker.LastMesssageId = message.Id;
+        spamTracker.LastMessageId = message.Id;
 
         spamTracker.PointTotal -= (message.Timestamp - spamTracker.DateTimeOffset).TotalSeconds * 2;
 
         if (spamTracker.PointTotal < 0)
             spamTracker.PointTotal = 0;
 
-        if (AddPresure(spamTracker, message, 10))
-            return new CheckSpamResult { IsSpam = true, Reason = "Automod: User Sent several messsages in a row." };
+        if (AddPressure(spamTracker, 10))
+            return new CheckSpamResult { IsSpam = true, Reason = "Auto mod: User Sent several messages in a row." };
 
-        if (AddPresure(spamTracker, message, message.Attachments.Count * 4.15))
+        if (AddPressure(spamTracker, message.Attachments.Count * 4.15))
             return new CheckSpamResult
             {
-                IsSpam = true, Reason = "Automod: User Sent messages with several attachments"
+                IsSpam = true, Reason = "Auto mod: User Sent messages with several attachments"
             };
 
-        if (AddPresure(spamTracker, message, message.Embeds.Count * 4.15))
-            return new CheckSpamResult { IsSpam = true, Reason = "Automod: User Sent messages with several Embeds." };
+        if (AddPressure(spamTracker, message.Embeds.Count * 4.15))
+            return new CheckSpamResult { IsSpam = true, Reason = "Auto mod: User Sent messages with several Embeds." };
 
-        if (AddPresure(spamTracker, message, message.Content.Length * 0.00625))
-            return new CheckSpamResult { IsSpam = true, Reason = "Automod: User sent too many characters." };
+        if (AddPressure(spamTracker, message.Content.Length * 0.00625))
+            return new CheckSpamResult { IsSpam = true, Reason = "Auto mod: User sent too many characters." };
 
-        if (AddPresure(spamTracker, message, message.Content.Split('\n').Length * 0.714))
+        if (AddPressure(spamTracker, message.Content.Split('\n').Length * 0.714))
             return new CheckSpamResult
             {
-                IsSpam = true, Reason = "Automod: User sent too many new lines in a message."
+                IsSpam = true, Reason = "Auto mod: User sent too many new lines in a message."
             };
 
-        if (AddPresure(spamTracker, message, (message.MentionedRoles.Count + message.MentionedUsers.Count) * 2.5))
-            return new CheckSpamResult { IsSpam = true, Reason = "Automod: User sent too many pings in a message." };
+        if (AddPressure(spamTracker, (message.MentionedRoles.Count + message.MentionedUsers.Count) * 2.5))
+            return new CheckSpamResult { IsSpam = true, Reason = "Auto mod: User sent too many pings in a message." };
 
         if (message.Content.Length > 0 && message.Content == spamTracker.MessageCache)
-            if (AddPresure(spamTracker, message, 10))
-                return new CheckSpamResult { IsSpam = true, Reason = "Automod: User sent too many duplicate Messages" };
+            if (AddPressure(spamTracker, 10))
+                return new CheckSpamResult { IsSpam = true, Reason = "Auto mod: User sent too many duplicate Messages" };
         spamTracker.MessageCache = message.Content;
         spamTracker.DateTimeOffset = message.Timestamp;
         return new CheckSpamResult { IsSpam = false };
     }
 
-    public static bool AddPresure(SpamTracker tracker, DiscordMessage message, double pointsToAdd)
+    private static bool AddPressure(SpamTracker tracker, double pointsToAdd)
     {
         tracker.PointTotal += pointsToAdd;
         return tracker.PointTotal > 60;
     }
 
-    public sealed record SpamTracker
+    private sealed record SpamTracker
     {
         public double PointTotal { get; set; }
         public string MessageCache { get; set; } = string.Empty;
-        public ulong LastMesssageId { get; set; }
+        public ulong LastMessageId { get; set; }
         public DateTimeOffset DateTimeOffset { get; set; }
     }
 
